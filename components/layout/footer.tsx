2c2c40292a0e473fab2e8d9657ad72a07e9a658b
masnurrm <sangmastahnm@gmail.com>
2024-12-28 18:40:59 +0700

    feat: base layout, navbar, footer

---
+++ components/layout/footer.tsx
@@ -0,0 +1,12 @@
+import { cn } from "@/lib/utils";
+import { footer } from "@/styles/font";
+
+export default function Footer() {
+    return (
+        <footer className={cn(footer.className, "py-3 text-center text-sm")}>
+            <p>
+                {new Date().getFullYear()} Nur Muhammad.
+            </p>
+        </footer>
+    );
+}
