03c0a6a3786e18a1c9b945b713a8c47a6c61e8c6
Nur Muhammad 'Ainul Yaqin <64957624+masnurrm@users.noreply.github.com>
2024-12-29 00:29:38 +0700

    Dev (#2)
    * feat: project config and env
    
    * feat: project config and env
    
    * feat: action for gitguardian
    
    * feat: base layout, navbar, footer
    
    * feat: mobile mode
    
    * fix: just executing the prettier rule, nothing change on codes
    
    * feat: adding home page and also canvas feature (thanks to wisesa.dev)
    
    * feat: adding capture feature on canvas doddle craeted (thanks to claude.ai haha)
    
    * fix: sorry, i forgot to reactivate the output in next.config.mjs
    
    * fix: sorry, i forgot to add color in canvas palette huhu
    
    * fix: adding padding for mobile
    
    * fix: double deployment
    
    * fix: merge conflict

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Canvas from '@/components/canvas';
 import MePhoto from '@/public/default/me-photo.svg';
+import { Doodle1, Doodle2 } from '@/components/doodle';
 import Image from 'next/image';
 import Link from 'next/link';
 
@@ -8,15 +9,21 @@ export default function Main() {
     <section className="relative flex h-full cursor-pencil items-center justify-center pb-6 dark:cursor-pencil-dark md:w-screen md:pb-0 md:pr-2.5">
       <div className="flex flex-col items-center justify-center md:flex-row md:justify-between">
         <div className="relative hidden w-[350px] place-items-center dark:grid md:w-[400px]">
-          <Image alt="photo" src={MePhoto} priority width={260} height={300} />
+          <Image alt="photo" src={MePhoto} priority width={300} height={300} />
+          <Doodle1 className="absolute bottom-0 stroke-[#ffd55a]/70" />
         </div>
         <div className="relative grid w-[350px] place-items-center dark:hidden md:w-[400px]">
-          <Image alt="photo" src={MePhoto} priority width={260} height={300} />
+          <Image alt="photo" src={MePhoto} priority width={300} height={300} />
+          <Doodle1 className="absolute bottom-0 stroke-[#ffd55a]/70" />
         </div>
         <div className="flex flex-col items-center justify-center space-y-4 md:ml-4 md:items-start">
           <h1 className="text-md mt-1 font-doodle text-3xlmd:text-left md:text-4xl px-4">
             It&apos;s me,
-            <span className="relative mx-1.5">masnurrm</span>
+            <span className="relative mx-1.5">
+              masnurrm
+              <Doodle2 className="absolute -bottom-4 -right-4 hidden w-32 stroke-[#ffd55a]/50 dark:block md:w-48 md:stroke-[#ffd55a]/70" />
+              <Doodle2 className="absolute -bottom-4 -right-4 block w-32 stroke-[#a3a3a3]/50 dark:hidden md:w-48 md:stroke-[#a3a3a3]/70" />
+            </span>
             &#128569; 🫵
           </h1>
           <p className="text-md max-w-[500px] text-center md:text-left md:text-lg px-4">
