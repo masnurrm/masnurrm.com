aaabc96926fb76c353202b9e4c401e1e5e5a4288
masnurrm <sangmastahnm@gmail.com>
2024-12-28 14:59:44 +0700

    feat: project config and env

---
+++ next.config.mjs
@@ -0,0 +1,23 @@
+import withPlaiceholder from "@plaiceholder/next";
+
+/** @type {import('next').NextConfig} */
+const config = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.scdn.co",
+        port: "",
+        pathname: "/image/**",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+};
+
+export default withPlaiceholder(config);
