07d54a4e154a06c75f50199cdacddd77bd906611
masnurrm <sangmastahnm@gmail.com>
2024-12-29 15:34:42 +0700

    feat: fixing manifest and next.config

--- next.config.mjs
+++ next.config.mjs
@@ -5,28 +5,45 @@ const config = {
   ...(process.env.DEPLOYMENT_ENV === 'cloudflare'
     ? {
         output: 'export',
+        async redirects() {
+          return [];
+        },
         images: {
           unoptimized: true,
+          remotePatterns: [
+            {
+              protocol: 'https',
+              hostname: 'i.scdn.co',
+              port: '',
+              pathname: '/image/**',
+            },
+            {
+              protocol: 'https',
+              hostname: 'images.unsplash.com',
+              port: '',
+              pathname: '/**',
+            },
+          ],
         },
       }
-    : {}),
-
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'i.scdn.co',
-        port: '',
-        pathname: '/image/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
+    : {
+        images: {
+          remotePatterns: [
+            {
+              protocol: 'https',
+              hostname: 'i.scdn.co',
+              port: '',
+              pathname: '/image/**',
+            },
+            {
+              protocol: 'https',
+              hostname: 'images.unsplash.com',
+              port: '',
+              pathname: '/**',
+            },
+          ],
+        },
+      }),
 };
 
 export default withPlaiceholder(config);
