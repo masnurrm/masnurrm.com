f1c17f36296a79bd410ffd17dc2157b74c6ae56d
masnurrm <sangmastahnm@gmail.com>
2024-12-29 15:01:25 +0700

    feat: adding manifest and error/not found page

---
+++ app/manifest.ts
@@ -0,0 +1,30 @@
+import type { MetadataRoute } from 'next';
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: 'masnurrm',
+    description:
+      'Pretending as a Software Engineer focusing on Cloud, DevSecOps, and Backend evelopment. Psst, talking about business too (lagi BU)!',
+    background_color: '#fff',
+    theme_color: '#fff',
+    display: 'standalone',
+    start_url: '/',
+    icons: [
+      {
+        src: '/favicon.ico',
+        sizes: 'any',
+        type: 'image/x-icon',
+      },
+      {
+        src: '/icon1.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: 'icon2.png',
+        sizes: '384x384',
+        type: 'image/png',
+      },
+    ],
+  };
+}
