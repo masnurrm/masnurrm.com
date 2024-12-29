88471d4f1a5b8b0adc1abe74fab234d422404171
masnurrm <sangmastahnm@gmail.com>
2024-12-30 01:46:07 +0700

    feat: sitemaps

---
+++ lib/hooks/use-postlib.ts
@@ -0,0 +1,94 @@
+import { Gist, Pre } from '@/components/code-block';
+import { Hr, Img, Quote } from '@/components/mdx-components';
+import matter from 'gray-matter';
+import { compileMDX } from 'next-mdx-remote/rsc';
+import fs from 'node:fs';
+import path from 'path';
+import readingTime from 'reading-time';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import rehypeKatex from 'rehype-katex';
+import rehypePrism from 'rehype-prism-plus';
+import rehypeSlug from 'rehype-slug';
+import codeTitle from 'remark-code-title';
+import remarkMath from 'remark-math';
+const root = process.cwd();
+
+const articlesPath = path.join(root, 'data/blog');
+
+export async function getFiles() {
+  return fs.readdirSync(articlesPath);
+}
+
+export async function getPostBySlug(slug: string) {
+  const articleDir = path.join(articlesPath, `${slug}.mdx`);
+  const source = fs.readFileSync(articleDir);
+  const { content, data } = matter(source);
+
+  const a = await compileMDX({
+    source: content,
+    options: {
+      mdxOptions: {
+        remarkPlugins: [codeTitle, remarkMath],
+        rehypePlugins: [
+          rehypePrism,
+          rehypeSlug,
+          [rehypeAutolinkHeadings, { behavior: 'wrap' }],
+          [rehypeKatex, { output: 'html' }],
+        ],
+      },
+    },
+    components: MdxComponent,
+  });
+
+  const body = a.content;
+
+  return {
+    body,
+    frontmatter: {
+      slug,
+      title: data.title,
+      description: data.description,
+      date: data.date,
+      image: data.image,
+      readingTime: readingTime(content),
+    },
+  };
+}
+
+interface BlogPost {
+  title: string;
+  description: string;
+  date: string;
+  image?: string;
+  slug: string;
+  readingTime: ReturnType<typeof readingTime>;
+}
+
+export async function getAllPost() {
+  const articles = fs.readdirSync(articlesPath);
+
+  return articles.reduce<BlogPost[]>((allArticles, articleSlug) => {
+    const source = fs.readFileSync(
+      path.join(articlesPath, articleSlug),
+      'utf-8',
+    );
+    const { data, content } = matter(source);
+
+    return [
+      {
+        ...data,
+        slug: articleSlug.replace('.mdx', ''),
+        readingTime: readingTime(content),
+      } as BlogPost,
+      ...allArticles,
+    ];
+  }, []);
+}
+
+const MdxComponent = {
+  hr: Hr,
+  pre: Pre,
+  Img,
+  Gist,
+  Quote,
+};
